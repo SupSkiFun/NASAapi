ac01f875b73287689c76a3fe9b267795ee984ad0
Joe Acosta <aumjoey@gmail.com>
2020-07-08 11:02:08 -0700

    new files

---
+++ Node.js/getNasa.js
@@ -0,0 +1,78 @@
+const superagent = require('superagent');
+const lodash = require('lodash');
+
+class NasaFun {
+    static getAverage(obj)
+    {
+        let avg = ( ( obj['estimated_diameter_min'] + obj['estimated_diameter_max'] ) / 2 ) ;
+        return lodash.round(avg , 5) ;
+    }
+
+    static makeObj(obj)
+    {
+        let cad = obj['close_approach_data'][0] ;
+        let lo = {
+            "Name" : obj['name'] ,
+            "ID" : obj['id'] ,
+            "PotentiallyHazardous" : obj['is_potentially_hazardous_asteroid'] ,
+            "CloseApproachDateTime" : cad['close_approach_date_full'] ,
+            "MissDistanceInKM" : lodash.round((cad['miss_distance']['kilometers']) , 5 ) ,
+            "AbsoluteMagnitudeH" : obj['absolute_magnitude_h'] ,
+            "DiameterInMeters" : NasaFun.getAverage(obj['estimated_diameter']['meters']) ,
+            "VelocityInKMpH" : lodash.round((cad['relative_velocity']['kilometers_per_hour']) , 5 ) ,
+            "URL" : obj['nasa_jpl_url']
+        }
+        return lo
+    }
+}
+
+function setConfig()
+{
+    let apj = "application/json" ;
+    let key = process.env.NASA_API_KEY ;
+    //let key = 'HARD_CODED_KEY_HERE'  if not using environment (line above)
+    url = 'https://api.nasa.gov/neo/rest/v1/feed' ;
+    hoy = makeDate() ;
+    msg = "Terminating.  Problem Accessing "+url+" for asteroid information:" ;
+    heads = {
+        "Content-Type": apj,
+        "Accept": apj
+    };
+    params = {
+        "api_key": key,
+        "start_date": hoy,
+        "end_date": hoy
+    };
+}
+
+function makeDate()
+{
+    return new Date().toISOString().split('T')[0];
+}
+
+async function getInfo() {
+    try
+    {
+        const res = await superagent.get(url).query(params).set(heads)  ;
+        return res.body['near_earth_objects'][hoy]
+    }
+    catch (err)
+    {
+        console.log(msg,"\n",err) ;
+        process.exit() ;
+    }
+}
+
+function procInfo(resp)
+{
+    let arr = []
+    for (let r = 0 ; r < resp.length ; r++)
+    {
+        arr.push(NasaFun.makeObj(resp[r]))
+    }
+    //lo = JSON.stringify(arr)
+    console.log(arr)
+}
+
+setConfig();
+getInfo().then(resp => procInfo(resp))
